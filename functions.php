d06357a21baffd02f07ded19ae885df1e45f435f
rgr <rogerdesouza.2010@hotmail.com>
2018-10-15 20:35:08 -0300

    Commit após alterações

--- functions.php
+++ functions.php
@@ -2,4 +2,16 @@
 function umaFuncao() {
 echo "Uma função que imprime".PHP_EOL;
 }
+function outraFuncao($nome){
+echo "Olá, {nome}!".PHP_EOL;
+}
+
+function outraFuncao($nome){
+echo "Olá,{$nome}!Eu sou umaFuncao!".PHP_EOL;
+}
+
+function novaFuncao($msg){
+echo "Sou uma funcao que recebe:{$msg}".PHP_EOL;
+}
+
 ?>
